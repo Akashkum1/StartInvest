5462c21888f68c8df1c9450c029bb3e0b6ee2231
akashneos <kumavat.akash.17ee1147@gmail.com>
2024-02-27 17:32:48 +0530

    added husky and path alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {vars: 'all', args: 'after-used', ignoreRestSiblings: false},
+    ],
+    'prettier/prettier': 'error',
+  },
 };
